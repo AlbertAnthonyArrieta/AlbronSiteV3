f2b23e748e77006c3f174ac45f07056d1c36659a
Albert Arrieta <albert.arrieta@uleth.ca>
2024-01-09 17:56:14 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import './components/sections/HeroSection/HeroSection.js';
+import Navbar from './components/common/navbar/Navigation.js';
+import HeroSection from './components/sections/HeroSection/HeroSection.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Container>
+      <HeroSection />
+      </Container>
     </div>
   );
 }
