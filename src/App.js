339d0989b093b82e9503539e76bf689946bb4aa4
Albert Arrieta <albert.arrieta@uleth.ca>
2024-01-11 18:25:07 -0700

    Projects Section Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './components/sections/HeroSection/HeroSection.js';
 import Navbar from './components/common/navbar/Navigation.js';
 import HeroSection from './components/sections/HeroSection/HeroSection.js';
 import AboutSection from './components/sections/AboutSection/AboutSection.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ServiceSection from './components/sections/ServiceSection/ServiceSection.js';
+import ProjectsSection from './components/sections/ProjectsSection/ProjectsSection.js';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <HeroSection />
       <AboutSection />
       <ServiceSection />
+      <ProjectsSection />
     </div>
   );
 }
---
+++ src/components/sections/ProjectsSection/ProjectsSection.js
@@ -0,0 +1,13 @@
+import Container from "react-bootstrap/esm/Container"
+
+export const ProjectsSection = () => {
+ return (
+    <div className="projects-section">
+        <Container>
+            <h1>Projects</h1>
+        </Container>
+    </div>
+ )
+}
+
+export default ProjectsSection
