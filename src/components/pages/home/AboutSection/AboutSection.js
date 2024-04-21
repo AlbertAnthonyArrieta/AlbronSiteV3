87e7a8dabdc12747508daa996d5a9f4bb6ad3e5b
Albert Arrieta <albert.arrieta@uleth.ca>
2024-04-20 21:50:51 -0600

    clean

--- src/components/pages/home/AboutSection/AboutSection.js
+++ src/components/pages/home/AboutSection/AboutSection.js
@@ -8,14 +8,14 @@ export const AboutSection = () => {
             <Container>
                 <div className="about-content">
                     <div about-image-container>
-                        <img className='about' alt="About Image" src={AboutImage} />
+                        <img className='about' alt="About us" src={AboutImage} />
                     </div>
                     <div about-text-container>
                         <h1>What We Do</h1>
                         <p>Our team delivers comprehensive engineering design and drafting services tailored to your enterprise, encompassing both minor structural undertakings and extensive facility developments.
                             <br /> <br />
                             With state-of-the-art computer-aided design (CAD) technologies at our disposal, we guarantee the precision and excellence of the design and drafting solutions we offer.</p>
-                        <BigButton type='secondary' text='Learn More' link='/about'/>
+                        <BigButton type='secondary' text='Learn More' link='/about' />
                     </div>
                 </div>
             </Container>
