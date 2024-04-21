b00638ca10c7cc9c7686cfb9561b3ae3be380dfd
Albert Arrieta <albert.arrieta@uleth.ca>
2024-04-20 21:17:00 -0600

    building

--- src/components/pages/Projects/PipelinesPage/PipelinesPage.js
+++ src/components/pages/Projects/PipelinesPage/PipelinesPage.js
@@ -13,7 +13,7 @@ export const FacilitiesPage = () => {
             <PageHeader title="Projects" image={BgImage} />
             <Container>
                 <div className="projectspage-content">
-                    <h1 className="projectspage-title">Come back next time!</h1>
+                    <h1 className="projectspage-title">Still Under Construction!</h1>
                     <div className="projects-container">
                     </div>
                 </div>
